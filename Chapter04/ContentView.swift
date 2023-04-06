eb9d357a2db152828d13f976f51deca783422841
Nick Eddington <neddington@cascadia.edu>
2023-04-06 16:25:08 -0700

    Updated changes completed

--- Chapter04/ContentView.swift
+++ Chapter04/ContentView.swift
@@ -12,18 +12,36 @@ struct ContentView: View {
     let age = 25
     
     var body: some View {
-        let message = "My name is \(name) and I am \(age) years old."
-        Text(message)
-            .font(.custom("Courier New", size: 18))
-            .foregroundColor(Color(red: 0.3, green: 0.5, blue: 0.8))
-            .bold()
-            .italic()
-            .underline()
-            .lineLimit(2)
-            .truncationMode(.middle)
-            .padding()
-        
-        
+
+        VStack {
+            let message = "My name is \(name) and I am \(age) years old."
+            Text(message)
+                .font(.custom("Courier New", size: 18))
+                .foregroundColor(Color(red: 0.3, green: 0.5, blue: 0.8))
+                .bold()
+                .italic()
+                .underline()
+                .lineLimit(2)
+                .truncationMode(.middle)
+                .padding()
+            
+            Label(message, systemImage: "person.fill")
+                .foregroundColor(.green)
+                .font(.headline)
+            
+            // Change the color of the icon to have two colors
+            Image(systemName: "heart.circle.fill")
+                .renderingMode(.original)
+                .foregroundColor(.pink)
+                .background(Color.blue)
+                .clipShape(Circle())
+                .frame(width: 100, height: 100)
+ //Icon Obtained from "https://icons8.com/icon/51974/xcode"
+                    Image("icons8-xcode-48")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                }
+                .padding()
     }
 }
 
